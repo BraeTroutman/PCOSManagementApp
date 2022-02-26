61505576693f266881ba5133fc050399a4ea1289
BraeTroutman <troupb18@wfu.edu>
2022-02-26 15:36:13 +0000

    added flatlist under connections searchbar

--- screens/Connections.js
+++ screens/Connections.js
@@ -1,55 +1,67 @@
 import React, { useState } from 'react';
-import { FlatList, TextInput, View, Text } from "react-native";
+import { StyleSheet, FlatList, TextInput, View, SafeAreaView, Text, StatusBar } from "react-native";
 import { SearchBar } from 'react-native-elements';
 
-const ItemView = ({item}) => {
-    return (
-      // Flat List Item
-      <Text style={styles.itemStyle} onPress={() => getItem(item)}>
-        {item.id}
-        {'.'}
-        {item.title.toUpperCase()}
-      </Text>
-    );
-}
-
-const ItemSeparatorView = () => {
-  return (
-    // Flat List Item Separator
-    <View
-      style={{
-        height: 0.5,
-        width: '100%',
-        backgroundColor: '#C8C8C8',
-      }}
-    />
-  );
-};
 const ConnectionsSearch = () => { 
     const [search, setSearch] = useState("") 
     const updateSearch = (search) => setSearch(search);
     return (
-	<View>
+	<SafeAreaView>
 	<SearchBar
 	    placeHolder="Type here..."
 	    onChangeText={updateSearch}
 	    value={search}
 	/>
-	<FlatList>
-	    data={fetch('https://jsonplaceholder.typicode.com/posts').then((response) => response.json())
-	    	.catch((error) => alert(error))}
-	    renderItem={ItemView}
-	    keyExtractor={(item, index) => index.toString()}
-	    ItemSeparatorComponent={ItemSeparatorView}
-	</FlatList>
-	</View>
+	<List/>	
+	</SafeAreaView>
     );
 };
 
 export default function ConnectionsScreen() {
     return (
       <View>
-     	  <ConnectionsSearch /> 
+     	<ConnectionsSearch/> 
       </View>
     );
 }
+
+const TESTDATA = fetch('https://jsonplaceholder.typicode.com/posts').then((response) => response.json).catch((error) => console.log(error));
+
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item',
+  },
+];
+
+const Item = ({ title }) => {
+  return (<View>
+    <Text>{title}</Text>
+  </View>);
+};
+
+const List = () => {
+  const renderItem = ({ item }) => {
+    return (<Item title={item.title} />);
+  };
+
+  return (
+    <SafeAreaView>
+      <FlatList
+        data={DATA}
+        renderItem={({ item }) => (
+	    <Text>{item.title}</Text>
+	)}
+      />
+    </SafeAreaView>
+  );
+}
+
