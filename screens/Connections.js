079139a40e8d4dd533d5e066a79e1b8c4c078c6a
BraeTroutman <troupb18@wfu.edu>
2022-02-26 14:48:01 +0000

    installed node-fetch

--- screens/Connections.js
+++ screens/Connections.js
@@ -1,23 +1,55 @@
 import React, { useState } from 'react';
-import { TextInput, View, Text } from "react-native";
+import { FlatList, TextInput, View, Text } from "react-native";
 import { SearchBar } from 'react-native-elements';
 
-const MyBar = () => { 
+const ItemView = ({item}) => {
+    return (
+      // Flat List Item
+      <Text style={styles.itemStyle} onPress={() => getItem(item)}>
+        {item.id}
+        {'.'}
+        {item.title.toUpperCase()}
+      </Text>
+    );
+}
+
+const ItemSeparatorView = () => {
+  return (
+    // Flat List Item Separator
+    <View
+      style={{
+        height: 0.5,
+        width: '100%',
+        backgroundColor: '#C8C8C8',
+      }}
+    />
+  );
+};
+const ConnectionsSearch = () => { 
     const [search, setSearch] = useState("") 
     const updateSearch = (search) => setSearch(search);
     return (
+	<View>
 	<SearchBar
 	    placeHolder="Type here..."
 	    onChangeText={updateSearch}
 	    value={search}
 	/>
+	<FlatList>
+	    data={fetch('https://jsonplaceholder.typicode.com/posts').then((response) => response.json())
+	    	.catch((error) => alert(error))}
+	    renderItem={ItemView}
+	    keyExtractor={(item, index) => index.toString()}
+	    ItemSeparatorComponent={ItemSeparatorView}
+	</FlatList>
+	</View>
     );
 };
 
 export default function ConnectionsScreen() {
     return (
       <View>
-     	  <MyBar /> 
+     	  <ConnectionsSearch /> 
       </View>
     );
 }
