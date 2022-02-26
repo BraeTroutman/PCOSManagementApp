dab123b3a4eccc246b02ffe90a24a0039c28db40
Racso7 <yuz17@wfu.edu>
2022-02-26 18:12:55 -0500

    yes

--- screens/Tracking.js
+++ screens/Tracking.js
@@ -1,10 +1,119 @@
 import * as React from 'react';
 import { View, Text } from "react-native";
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph,
+  StackedBarChart
+} from 'react-native-chart-kit'
+import {LocaleConfig} from 'react-native-calendars';
+
+LocaleConfig.locales['fr'] = {
+  monthNames: [
+    'Janvier',
+    'Février',
+    'Mars',
+    'Avril',
+    'Mai',
+    'Juin',
+    'Juillet',
+    'Août',
+    'Septembre',
+    'Octobre',
+    'Novembre',
+    'Décembre'
+  ],
+  monthNamesShort: ['Janv.', 'Févr.', 'Mars', 'Avril', 'Mai', 'Juin', 'Juil.', 'Août', 'Sept.', 'Oct.', 'Nov.', 'Déc.'],
+  dayNames: ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'],
+  dayNamesShort: ['Dim.', 'Lun.', 'Mar.', 'Mer.', 'Jeu.', 'Ven.', 'Sam.'],
+  today: "Aujourd'hui"
+};
+LocaleConfig.defaultLocale = 'fr';
 
 export default function TrackingScreen() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text style={{fontSize:16,fontWeight:'700'}}>Tracking</Text>
+        <Calendar
+          // Initially visible month. Default = now
+          current={'2012-03-01'}
+          // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
+          minDate={'2012-05-10'}
+          // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
+          maxDate={'2012-05-30'}
+          // Handler which gets executed on day press. Default = undefined
+          onDayPress={day => {
+            console.log('selected day', day);
+          }}
+          // Handler which gets executed on day long press. Default = undefined
+          onDayLongPress={day => {
+            console.log('selected day', day);
+          }}
+          // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
+          monthFormat={'yyyy MM'}
+          // Handler which gets executed when visible month changes in calendar. Default = undefined
+          onMonthChange={month => {
+            console.log('month changed', month);
+          }}
+          // Hide month navigation arrows. Default = false
+          hideArrows={true}
+          // Replace default arrows with custom ones (direction can be 'left' or 'right')
+          renderArrow={direction => <Arrow />}
+          // Do not show days of other months in month page. Default = false
+          hideExtraDays={true}
+          // If hideArrows = false and hideExtraDays = false do not switch month when tapping on greyed out
+          // day from another month that is visible in calendar page. Default = false
+          disableMonthChange={true}
+          // If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday
+          firstDay={1}
+          // Hide day names. Default = false
+          hideDayNames={true}
+          // Show week numbers to the left. Default = false
+          showWeekNumbers={true}
+          // Handler which gets executed when press arrow icon left. It receive a callback can go back month
+          onPressArrowLeft={subtractMonth => subtractMonth()}
+          // Handler which gets executed when press arrow icon right. It receive a callback can go next month
+          onPressArrowRight={addMonth => addMonth()}
+          // Disable left arrow. Default = false
+          disableArrowLeft={true}
+          // Disable right arrow. Default = false
+          disableArrowRight={true}
+          // Disable all touch events for disabled days. can be override with disableTouchEvent in markedDates
+          disableAllTouchEventsForDisabledDays={true}
+          // Replace default month and year title with custom one. the function receive a date as parameter
+          renderHeader={date => {
+            /*Return JSX*/
+          }}
+          // Enable the option to swipe between months. Default = false
+          enableSwipeMonths={true}
+        />
+        <Text>
+          Line Chart
+        </Text>
+        <LineChart
+          data={linedata}
+          width={Dimensions.get('window').width} // from react-native
+          height={220}
+          yAxisLabel={'$'}
+          chartConfig={{
+            backgroundColor: '#e26a00',
+            backgroundGradientFrom: '#fb8c00',
+            backgroundGradientTo: '#ffa726',
+            decimalPlaces: 2, // optional, defaults to 2dp
+            color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+            style: {
+              borderRadius: 16
+            }
+          }}
+          bezier
+          style={{
+            marginVertical: 8,
+            borderRadius: 16
+          }}
+        />
+
       </View>
     );
   }
