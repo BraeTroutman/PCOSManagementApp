90f81a78f686339583fe533d65c6b312c2326b7d
Racso7 <yuz17@wfu.edu>
2022-03-03 18:18:33 -0500

    added textinput in report

--- screens/Report.js
+++ screens/Report.js
@@ -2,13 +2,18 @@ import * as React from 'react';
 import { Button, View, Text } from "react-native";
 //import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { TextInput } from 'react-native-gesture-handler';
 
 const Stack = createNativeStackNavigator();
 
 export default function ReportScreen({ navigation }) {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-	    
+          <Text> Welcome to daily report! </Text>
+          <TextInput placeholder='Enter your weight'/>
+          <TextInput placeholder='Enter your anxiety level'/>
+          <TextInput placeholder='Starting day of your cycle'/>
+          <TextInput placeholder='Ending day of your cycle'/>
       </View>
     );
 }
