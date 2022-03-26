df981f8e7bf1066dd4e6fb30e5cd3bda67bada3b
BraeTroutman <braetroutman@gmail.com>
2022-03-26 18:44:58 -0400

    added some filler xml to render

---
+++ components/Post.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { 
+    View, 
+    Text,
+} from 'react-native';
+
+export default function Post() {
+    return (
+        <View>
+            <Text>This is a post!</Text>
+        </View>
+    );
+}
