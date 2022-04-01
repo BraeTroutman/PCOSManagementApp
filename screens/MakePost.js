db75237c491e23fe54ecc195a2a31c94eab65cd7
BraeTroutman <braetroutman@gmail.com>
2022-03-31 23:05:48 -0400

    add functionality to take or load pictures from gallery

--- screens/MakePost.js
+++ screens/MakePost.js
@@ -1,9 +1,37 @@
 import React, { useState } from 'react';
-import { StyleSheet, Button, View, Text } from 'react-native';
+import { StyleSheet, Button, View, Text, Image } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function MakePostScreen() {
     const [selected, setSelected] = useState("Experiencer");
+    const [image, setImage] = useState(null);
+
+    const pickImage = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect: [4,3],
+            quality: 1,
+        });
+        
+        console.log(result);
+
+        if (!result.cancelled) setImage(result.uri);
+    }
+    const takeImage = async () => {
+        let result = await ImagePicker.launchCameraAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 1,
+        });
+
+        console.log(result);
+
+        if (!result.cancelled) setImage(result.uri);
+    }
+
     return (
         <View>
             <Picker
@@ -11,10 +39,13 @@ export default function MakePostScreen() {
                 style={styles.picker}
                 onValueChange={(val, idx) => setSelected(val)}
             >
-                <Picker.Item label="Experiencer" value="Experiencer"/>
-                <Picker.Item label="Mentor" value="Mentor"/>
-                <Picker.Item label="Friend" value="Friend"/>
+                <Picker.Item label="Recommendation" value="Recommendation"/>
+                <Picker.Item label="Image" value="Image"/>
+                <Picker.Item label="Story" value="Story"/>
             </Picker>
+            <Button title="load picture" onPress={pickImage}/>
+            <Button title="take picture" onPress={takeImage}/>
+            {image && <Image source={{uri: image}} style={styles.image}/>}
         </View>
     );
 }
@@ -22,4 +53,8 @@ export default function MakePostScreen() {
 const styles = StyleSheet.create({
     page: {},
     picker: {},
+    image: {
+        height: 200,
+        width: 200,
+    }
 })
