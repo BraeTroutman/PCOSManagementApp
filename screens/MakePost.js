06c8c6dd1f8f4fb1f9afbac87d7f0d1ad7167852
BraeTroutman <braetroutman@gmail.com>
2022-03-31 22:44:52 -0400

    install and incorporate react-native-picker

--- screens/MakePost.js
+++ screens/MakePost.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Button, View, Text } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
 
 export default function MakePostScreen() {
+    const [selected, setSelected] = useState("Experiencer");
     return (
-        <View></View>
+        <View>
+            <Picker
+                selectedValue={selected}
+                style={styles.picker}
+                onValueChange={(val, idx) => setSelected(val)}
+            >
+                <Picker.Item label="Experiencer" value="Experiencer"/>
+                <Picker.Item label="Mentor" value="Mentor"/>
+                <Picker.Item label="Friend" value="Friend"/>
+            </Picker>
+        </View>
     );
 }
 
 const styles = StyleSheet.create({
     page: {},
+    picker: {},
 })
