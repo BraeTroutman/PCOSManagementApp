267fd937f98fcc69c0dcba678b7af2ebd9701745
BraeTroutman <braetroutman@gmail.com>
2022-04-01 12:58:15 -0400

    create components for different post types

---
+++ components/MakeImagePost.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Text, View} from 'react-native';
+
+export default function ImagePost() {
+    return (<View><Text>Image Post</Text></View>);
+}
---
+++ components/MakeReccPost.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Text, View} from 'react-native';
+
+export default function ReccPost() {
+    return (<View><Text>Recc Post</Text></View>);
+}
