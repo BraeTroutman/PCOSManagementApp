93a4138ff2c58b3f784ea95c2eb0f0cda1575c40
BraeTroutman <braetroutman@gmail.com>
2022-04-02 15:36:54 -0400

    define MakePost screen

--- screens/MakePost.js
+++ screens/MakePost.js
@@ -1,11 +1,22 @@
 import React, { useState } from 'react';
-import { StyleSheet, Button, View, Text, Image } from 'react-native';
+import { 
+    StyleSheet, 
+    Button, 
+    View, 
+    Text, 
+    TextInput, 
+    Image,
+    ScrollView,
+    KeyboardAvoidingView,
+    TouchableOpacity,
+ } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
 import * as ImagePicker from 'expo-image-picker';
 
-export default function MakePostScreen() {
+export default function MakePostScreen({navigation}) {
     const [selected, setSelected] = useState('IMAGE');
     const [image, setImage] = useState(null);
+    const [text, setText] = useState('');
 
     const pickImage = async () => {
         let result = await ImagePicker.launchImageLibraryAsync({
@@ -23,19 +34,17 @@ export default function MakePostScreen() {
         let result = await ImagePicker.launchCameraAsync({
             mediaTypes: ImagePicker.MediaTypeOptions.All,
             allowsEditing: true,
-            aspect: [4, 3],
+            aspect: [1, 1],
             quality: 1,
         });
-
-        console.log(result);
 
         if (!result.cancelled) setImage(result.uri);
     }
 
     return (
-        <View>
+        <View style={styles.page} behavior='padding'>
             <Picker
-                selectedValue={PostType}
+                selectedValue={selected}
                 style={styles.picker}
                 onValueChange={(val, idx) => setSelected(val)}
             >
@@ -43,15 +52,81 @@ export default function MakePostScreen() {
                 <Picker.Item label="Image" value="IMAGE"/>
                 <Picker.Item label="Story" value="STORY"/>
             </Picker>
+            <KeyboardAvoidingView style={styles.content}>
+            {
+                selected === 'IMAGE'
+                ? <View>
+                    <Text style={styles.typeTitle}>IMAGE</Text>
+                    <Button title="Load Photo" onPress={pickImage}/>
+                    <Button title="Take Photo" onPress={takeImage}/>
+                </View>
+                : selected === 'RECC'
+                ? <Text style={styles.typeTitle}>RECC</Text>
+                : <Text style={styles.typeTitle}>STORY</Text>
+            }
+            <TextInput
+                style={styles.text}
+                multiline={true}
+                editable
+                onChangeText={setText}
+                placeholder="You're message here..."
+            />
+            <View style={styles.cancelSubmit}>
+                <TouchableOpacity style={styles.cancel} onPress={() => navigation.goBack()}><Text>cancel</Text></TouchableOpacity> 
+                <TouchableOpacity style={styles.submit} onPress={() => navigation.goBack()}><Text>Post</Text></TouchableOpacity>
+            </View>
+            </KeyboardAvoidingView>
+            <View style={{flex: 1}}/>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    page: {},
-    picker: {},
+    page: {
+        flex: 1,
+        padding: 10,
+    },
+    picker: {
+        flex: 1,
+    },
+    imageContainer: {
+        padding: 10,
+    },
     image: {
-        height: 200,
-        width: 200,
+    },
+    text: {
+        flex: 13,
+        padding: 10,
+        fontSize: 18,
+    },
+    typeTitle: {
+        paddingTop: 10,
+        fontSize: 24,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    content: {
+        backgroundColor: 'white',
+        height: '75%',
+        borderRadius: 10,
+        flex: 20,
+    },
+    cancelSubmit: {
+        flex: 2,
+        flexDirection: 'row',
+    },
+    cancel: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'lightgrey',
+        borderBottomLeftRadius: 10,
+    },
+    submit: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'white',
+        borderBottomRightRadius: 10,
     }
 })
