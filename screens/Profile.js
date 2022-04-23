76bcaeae0fb8ddc1b42d420a9ef117c78489afe9
BraeTroutman <braetroutman@gmail.com>
2022-04-23 14:14:14 -0400

    final commit -Brae

--- screens/Profile.js
+++ screens/Profile.js
@@ -5,76 +5,77 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import ConnectionsScreen from './Connections';
 import Users from '../constants/Users';
 
-export default function ProfileScreen({ navigation }) {
-    const styles = StyleSheet.create({
-	header: {
-		flex: 1,
-		flexDirection: 'row',
-		alignItems: 'center',
-		justifyContent: 'flex-start',
-		width: '100%',
-		padding: 10,
-	},
-	body: {
-		flex: 4,
-	},
-	profPhoto: {
-		height: 100,
-		width: 100,
-		borderRadius: 50,
-	},
-	user: {
-	    fontSize: 30,
-	    fontWeight: '700',
-	    margin: 10,
-	},
-	role: {
-	    fontSize: 20,
-	    fontWeight: '500',
-	    margin: 10,
-	},
-	bio: {
-	    fontSize: 25,
-	    fontWeight: '500',
-	    margin: 10,
-	},
-	activities: {
-	    fontSize: 15,
-	    fontWeight: '700',
-	    margin: 10,
-	},
-	description: {
-	    fontSize: 15,
-	    fontWeight: '300',
-	    margin: 10,
-	},
-	myButton: {
-	    margin: 20,
-	}
-    });
+export default function ProfileScreen({navigation}) {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'flex-start' }}>
-	<View style={styles.header}>
-	<Image 
-		style={styles.profPhoto}
-	    source={{uri: Users[3].picture.large}}
-	/>
-	<Text style={styles.user}>{Users[3].name.first} {Users[3].name.last}</Text>
+	<View style={styles.container}>
+		<View style={styles.header}>
+			<Image
+				source={{uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/VAN_CAT.png/440px-VAN_CAT.png"}}
+				style={styles.profilePhoto}
+		         />
+			   <Text style={styles.name}>Steve Thompson</Text>
+   			   <Button style={styles.button} title="follow +"/>
+			 </View>
+			 <View style={styles.body}>
+				<Text style={styles.description}>This user is a Supporter</Text>
+                        <Text style={styles.description}>Biography:</Text>
+                        <Text style={styles.description}>Hello there! I am steve, and I am here to support my wife, who was diagnosed with PCOS eight months ago. Looking forward to meeting fellow supporters and learning new things! :)</Text>
+				<View style={styles.buttons}>
+                          <Button style={styles.button} title="My connections" onPress={()=>navigation.navigate('Connections')}/>
+				  <Button style={styles.button} title="Edit Profile"/>
+				  <Button style={styles.button} title="Privacy Settings"/>
+				  <Button style={styles.button} title="Edit Preferences"/>
+			</View>
+		</View>
 	</View>
-	<View style={styles.body}>
-	<Text style={styles.role}>This user is a(n) {Users[3].role}</Text>
-	<Button style={styles.myButton}  title="My Connections" onPress={() => navigation.navigate('Connections')} />
-	<Button style={styles.myButton}  title="Edit Profile"/>
-	<Button style={styles.myButton}  title="Edit Privacy Settings"/>
-	<Button style={styles.myButton}  title="Edit Sharing Preferences"/>
-	<Button style={styles.myButton}  title="Edit Content Preferences"/>
-	<Button style={styles.myButton}  title="Edit Notification Preferences"/>
-
-	<Text style={styles.bio}>Bio...</Text>
-	<Text style={styles.activities}>Recent activity...</Text>
-	<Text style={styles.description}>This is an optional description lorem ipsum dolor sit amet, consectetur adipicking elit, sed do eriusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut</Text>
-	</View>
-      </View>
     );
-}
+ }
 
+      const styles = StyleSheet.create({
+		container: {
+		  flex: 1,
+		  flexDirection: 'column',
+		},
+		header: {
+		  flex: 1,
+		  flexDirection: 'row',
+		  alignItems: 'center',
+		  margin: 10,
+		  marginTop: 30,
+		  borderBottomColor: 'black',
+		  borderBottomWidth: 1,
+		  padding: 10,
+		},
+		body: {
+		  flex: 6,
+		  flexDirection: 'column',
+		  alignItems: 'center',
+		},
+		profilePhoto: {
+		  height: 100,
+		  width: 100,
+		  borderRadius: 50,
+		},
+		name: {
+		  fontWeight: 'bold',
+		  paddingLeft: 20,
+		  fontSize: 20,
+		},
+		description: {
+		  padding: 10,
+		},
+		bio: {
+		  padding: 10,
+		},
+		button: {
+		  width: 100,
+		},
+		buttons: {
+		  flex: 10,
+		  flexDirection: 'column',
+		  width: '100%'
+		}
+	});
+	
+
+  
